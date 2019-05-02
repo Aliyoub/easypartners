439ff379d586815c068ad93ddb6d08f49f0f001a
aliyoub <abisocial@hotmail.com>
2019-05-02 20:40:38 +0200

    fonctionnel au minimum retrait données sensibles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,7 @@ export class AppComponent {
   currentUrl: string;
   constructor(private router: Router, private location: Location ) {
     const config = {
-      apiKey: 'AIzaSyB0g4s9992Sl0U3YWm6hPlZ17qe-QND0Sg',
-      authDomain: 'easy-partnership-2675.firebaseapp.com',
-      databaseURL: 'https://easy-partnership-2675.firebaseio.com',
-      projectId: 'easy-partnership-2675',
-      storageBucket: 'easy-partnership-2675.appspot.com',
-      messagingSenderId: '912086837318'
+      /* *** */
     };
     firebase.initializeApp(config);
 
